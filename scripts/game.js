ba1ca9ccda4dd6785bcf34e081d3d6e2a2f18731
Georgios Kopanidis <kopanidis@me.com>
2023-12-29 13:33:36 +0000

    Add new game function and tests

--- scripts/game.js
+++ scripts/game.js
@@ -1,8 +1,19 @@
 let game = {
-    score: 0,
     currentGame: [],
     playerMoves: [],
-    choices: ["button1", "button2", "button3", "button4"],
+    score: 0,
+    choices: ["button1", "button2", "button3", "button4"]
+};
+
+function newGame() {
+    game.currentGame = [];
+    game.playerMoves = [];
+    game.score = 0;
+    showScore();
 }
 
-module.exports = { game };
+function showScore() {
+    document.getElementById("score").innerText = game.score;
+}
+
+module.exports = { game, newGame, showScore };
